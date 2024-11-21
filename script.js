3f4580e93552f26f8220c4e3934248d4fc4940f1
Geza Csosz <geza.csosz@gmail.com>
2024-11-21 15:38:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 
-function getRandomImage() {
-    var randomImage = new Array();
-    randomImage[0] = "images/1.jpg";
-    randomImage[1] = "images/2.jpg";
-    randomImage[2] = "images/3.jpg";
-    randomImage[3] = "images/4.jpg";
+// Array of image file paths
+const images = [
+    './assets/images/1.png',
+    './assets/images/2.png',
+    './assets/images/3.png',
+    './assets/images/4.png',
+];
 
-    var number = Math.floor(Math.random() * randomImage.length);
-    
-    return document.getElementById('result').innerHTML = '<img src="' + randomImage[number]+'" />'; 
-} 
+// Function to randomly select and display an image
+function displayRandomImage() {
+    // Get a random index
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
+
+    // Get the container element
+    const imageContainer = document.getElementById('image-container');
+
+    // Create an image element
+    const imgElement = document.createElement('img');
+    imgElement.src = selectedImage;
+    imgElement.alt = `Random image ${randomIndex + 1}`;
+
+    // Clear previous image and append the new one
+    imageContainer.innerHTML = '';
+    imageContainer.appendChild(imgElement);
+}
+
+// Display a random image when the page loads
+window.onload = displayRandomImage;
 
 
 
